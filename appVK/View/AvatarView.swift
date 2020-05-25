f7767faf037e13a9488ba705d7a8516c754258e7
sergey <desyak@ya.ru>
2020-05-25 18:06:48 +0300

    add shadow and contour ellipse

---
+++ appVK/View/AvatarView.swift
@@ -0,0 +1,27 @@
+//
+//  AvatarView.swift
+//  appVK
+//
+//  Created by Sergey Desyak on 25.05.2020.
+//  Copyright © 2020 Sergey Desyak. All rights reserved.
+//
+
+import UIKit
+@IBDesignable
+
+class AvatarView: UIView {
+    @IBOutlet var shadowView: UIView!
+    // @IBOutlet var friendImageView: UIImageView!
+    
+    
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+
+}
--- appVK/View/FriendCell.swift
+++ appVK/View/FriendCell.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class FriendCell: UITableViewCell {
 
+    @IBOutlet var avatarView: AvatarView!
     @IBOutlet var friendImageView: UIImageView!
-    
     @IBOutlet var friendNameLabel: UILabel!
     
+    @IBOutlet var heartLike: HeartView!
+    @IBInspectable var shadowColor = UIColor.black
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        avatarView.shadowView.layer.shadowColor = shadowColor.cgColor
+        avatarView.shadowView.layer.shadowOffset = .zero
+        avatarView.shadowView.layer.shadowRadius = 7
+        avatarView.shadowView.layer.shadowOpacity = 0.8
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        friendImageView.layer.cornerRadius = bounds.height/2
+        avatarView.shadowView.layer.cornerRadius = bounds.height/2
+    }
     
 }
