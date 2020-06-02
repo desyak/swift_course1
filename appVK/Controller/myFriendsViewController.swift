8d29931e9004891981900cfa4a5b8c57006696b5
sergey <desyak@ya.ru>
2020-06-02 18:56:36 +0300

    made Search

--- appVK/Controller/myFriendsViewController.swift
+++ appVK/Controller/myFriendsViewController.swift
@@ -10,15 +10,16 @@ import UIKit
 
 class myFriendsViewController: UITableViewController, UISearchResultsUpdating {
     
-    var searchResults = [Friend]()
-       let searchController = UISearchController(searchResultsController: nil)
+    
+    private var searchResults = [Friend]()
+    let searchController = UISearchController(searchResultsController: nil)
 
     
     
     
     
     
-    var friends = [
+    let friends = [
         Friend(image: UIImage(named: "pikachu")!, name: "Иван Иванов"),
         Friend(image: UIImage(named: "pikachu")!, name: "Петя Петров"),
         Friend(image: UIImage(named: "pikachu")!, name: "Вася Васин"),
@@ -31,7 +32,10 @@ class myFriendsViewController: UITableViewController, UISearchResultsUpdating {
         super.viewDidLoad()
         
         searchController.searchResultsUpdater = self
-        self.definesPresentationContext = true
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
 
         // Place the search bar in the table view's header.
         self.tableView.tableHeaderView = searchController.searchBar
@@ -39,23 +43,26 @@ class myFriendsViewController: UITableViewController, UISearchResultsUpdating {
         // Set the content offset to the height of the search bar's height
         // to hide it when the view is first presented.
         self.tableView.contentOffset = CGPoint(x: 0, y: searchController.searchBar.frame.height)
+         navigationController?.setNavigationBarHidden(false, animated: true)
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        navigationController?.setNavigationBarHidden(false, animated: true)
-        
-    }
+//    override func viewDidAppear(_ animated: Bool) {
+//        super.viewDidAppear(animated)
+//
+//        navigationController?.setNavigationBarHidden(false, animated: true)
+//
+//    }
 
     func filterContent(for searchText: String) {
-        searchResults = friends.filter({( image: UIImage, name: String) -> Bool in
-            let match = name.range(of: searchText)
-            return (match != nil)
+        searchResults = friends.filter({( friend: Friend) -> Bool in
+            let match = friend.name.range(of: searchText)
+            return match != nil
         })
+        
+        
     }
 
-    func updateSearchResults(for searchController: UISearchController) {
+     func updateSearchResults(for searchController: UISearchController) {
         if let searchText = searchController.searchBar.text {
             filterContent(for: searchText)
             tableView.reloadData()
@@ -66,28 +73,32 @@ class myFriendsViewController: UITableViewController, UISearchResultsUpdating {
     
     // MARK: - Table view data source
 
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        return 1
-//    }
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return friends.count
+        return searchController.isActive ? searchResults.count : friends.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let entry = searchController.isActive ? searchResults[indexPath.row] : friends[indexPath.row]
+        
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "FriendCell", for: indexPath) as? FriendCell else {
             preconditionFailure("FriendCell cannot be dequeued")
         }
 
         print("Section: " + String(indexPath.section) + ", row: " + String(indexPath.row))
-        let friendName = friends[indexPath.row].name
-        let friendImage = friends[indexPath.row].image
-        cell.friendNameLabel.text = friendName
-        cell.friendImageView.image = friendImage
+        let friendName = entry.name
+        let friendImage = entry.image
+        cell.friendNameLabel?.text = friendName
+        cell.friendImageView?.image = friendImage
         
 
         return cell
     }
+    
+
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
