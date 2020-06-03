2d17cea289bde1363f02878f95497bfd774498ed
sergey <desyak@ya.ru>
2020-06-03 18:33:02 +0300

    add

--- appVK/Controller/myNewsViewController.swift
+++ appVK/Controller/myNewsViewController.swift
@@ -38,11 +38,7 @@ class myNewsViewController: UITableViewController {
         self.tableView.contentOffset = CGPoint(x: 0, y: searchController.searchBar.frame.height)
         navigationController?.setNavigationBarHidden(false, animated: true)
         
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
 
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     
@@ -54,56 +50,7 @@ class myNewsViewController: UITableViewController {
         
     // MARK: - Table view data source
 
-    
 
-    /*
-
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
 }
@@ -125,6 +72,7 @@ extension myNewsViewController {
         }
 
         let cellnews = searchController.isActive ? searchResults[indexPath.row] : news[indexPath.row]
+
         cell.textNews.text = cellnews.textNews
         cell.titleNews.text = cellnews.title
         return cell
